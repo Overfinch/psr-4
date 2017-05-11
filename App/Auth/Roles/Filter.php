5e536529caf0fc985a5866743a8ec561eb880ffe
Overfinch <bundichmail@gmail.com>
2017-05-11 16:54:33 +0300

    PSR-4
    PSR-4

---
+++ App/Auth/Roles/Filter.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Auth\Roles;
+
+class Filter {
+    function __construct(){
+        echo "Filter created";
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once('vendor/autoload.php');
+
+use App\Auth\Roles\Filter;
+
+$filter = new Filter();
